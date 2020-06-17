ec710a5f02d1a9ad474cda406c1ebbf51edc79c0
zjiaming <jimmytsangfly@gmail.com>
2020-06-18 01:04:49 +0800

    创建包名，需改pom文件，添加需要使用的库

---
+++ src/main/java/com/zouwei/firstweb/FirstWebApplication.java
@@ -0,0 +1,13 @@
+package com.zouwei.firstweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebApplication.class, args);
+	}
+
+}
